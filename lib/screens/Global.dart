8530310f004f8b2f13787d5afc3222fc140a0f3e
Deepanshi Dhiman <deepanshi045btcseai23@igdtuw.ac.in>
2024-11-04 15:48:08 +0530

    Updated the file with necessary changes

---
+++ lib/screens/Global.dart
@@ -0,0 +1,21 @@
+//This file stores all of the main global variables for now, untll we figure out a better alternative.
+
+library globals;
+
+String fitnessLevel = "easy";
+
+String disabillity = "";
+
+String goal = "";
+
+String assistiveDevice = "";
+
+String activityEnjoy = "";
+
+String time = "";
+
+String restrictions = "";
+
+String motivationalMessages = "";
+
+String communityChallenge = "";
